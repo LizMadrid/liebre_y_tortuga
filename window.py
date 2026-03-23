29cbaf973c9a936164468196955db71533a5e2ac
LizMadrid <liz.mad1025@gmail.com>
2026-03-23 12:08:51 -0600

    Dos

--- window.py
+++ window.py
@@ -17,4 +17,43 @@ class Window(QWidget):
         self.imgLiebre.move(10,50)
 
         #Imagen Tortuga
-        
+        self.imgTortuga = QLabel(self)
+        self.imgTortuga.setPixmap(QPixmap("./img/tortuga.png"))
+        self.imgTortuga.setScaledContents(True)
+        self.imgTortuga.resize(100, 100)
+        self.imgTortuga.move(10, 150)
+
+        #Botón iniciar
+        self.btnIniciar = QPushButton("Iniciar", self)
+        self.btnIniciar.move(350, 300)
+        self.btnIniciar.clicked.connect(self.iniciar)
+
+        #Crear subprocesos
+        self.pLiebre = SubProceso(self.imgLiebre, 500)
+        self.pTortuga = SubProceso(self.imgTortuga, 5000)
+
+        #Conectar señales
+        self.pLiebre.actualizar.connect(self.moverLiebre)
+        self.pTortuga.actualizar.connect(self.moverTortuga)
+
+    def iniciar(self):
+        if self.btnIniciar.text() == "Iniciar":
+            self.btnIniciar.setStyleSheet("background-color: green")
+            self.btnIniciar.setText("Detener")
+            self.pLiebre.start()
+            self.pTortuga.start()
+        else:
+            self.btnIniciar.setStyleSheet("background-color: red")
+            self.btnIniciar.setText("Iniciar")
+            self.pLiebre.stop() 
+            self.pTortuga.stop()
+
+    def moverLiebre(self, x, y):
+        self.imgLiebre.move(x, y)
+
+    def moverTortuga(self, x, y):
+        self.imgTortuga.move(x, y)                   
+
+
+
+
